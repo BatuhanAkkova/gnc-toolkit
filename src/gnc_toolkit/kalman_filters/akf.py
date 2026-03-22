f31d8f1800b31909e06610bf417d98afe6056f9c
Batuhan Akkova <batuhanakkova1@gmail.com>
2026-03-22 22:00:22 +0300

    Implemented Myers-Tapley algorithm for estimation and prediction update.

--- src/gnc_toolkit/kalman_filters/akf.py
+++ src/gnc_toolkit/kalman_filters/akf.py
@@ -1,3 +1,7 @@
+"""
+Adaptive Kalman Filter (AKF) with online covariance estimation (Myers-Tapley).
+"""
+
 import numpy as np
 from gnc_toolkit.kalman_filters.kf import KF
 
@@ -18,10 +22,11 @@ class AKF(KF):
         super().__init__(dim_x, dim_z)
         self.N = window_size
         self.innovations = []
-        self.S_list = []
         self.H_list = []
         self.P_minus_list = []
-        self.predict_list = [] # Fx
+        self.FPF_T_list = []
+        self.dx_list = []
+        self.P_plus_list = []
 
     def predict(self, u=None, F=None, Q=None, B=None):
         """Standard predict step, but stores values for adaptation."""
@@ -29,72 +34,70 @@ class AKF(KF):
         if Q is None: Q = self.Q
         if B is None: B = self.B
         
-        self.P_minus_list.append(self.P.copy())
-        # Store predicted state without noise/update for Q estimation
-        if B is not None and u is not None:
-            self.predict_list.append(np.dot(F, self.x) + np.dot(B, u))
-        else:
-            self.predict_list.append(np.dot(F, self.x))
-            
+        # Store F * P_{k-1|k-1} * F^T for Q estimation
+        self.FPF_T_list.append(np.dot(np.dot(F, self.P), F.T))
+        
         super().predict(u, F, Q, B)
+        
+        # Store P_{k|k-1} for R estimation
+        self.P_minus_list.append(self.P.copy())
         
         if len(self.P_minus_list) > self.N:
             self.P_minus_list.pop(0)
-            self.predict_list.pop(0)
+            self.FPF_T_list.pop(0)
 
     def update(self, z, H=None, R=None):
         """Update step with online R and Q adaptation."""
         if H is None: H = self.H
         if R is None: R = self.R
         
-        # Standard update first to get innovation
-        # y = z - Hx (done inside super().update, but we need it for adaptation)
+        x_minus = self.x.copy()
         y = z - np.dot(H, self.x)
-        S = np.dot(np.dot(H, self.P), H.T) + R
+        
+        super().update(z, H, R)
+        
+        dx = self.x - x_minus
         
         self.innovations.append(y)
-        self.S_list.append(S)
         self.H_list.append(H)
+        self.dx_list.append(dx)
+        self.P_plus_list.append(self.P.copy())
         
         if len(self.innovations) > self.N:
             self.innovations.pop(0)
-            self.S_list.pop(0)
             self.H_list.pop(0)
+            self.dx_list.pop(0)
+            self.P_plus_list.pop(0)
             
         # Perform adaptation if we have enough samples
         if len(self.innovations) >= self.N:
             self._adapt_noise_covariances()
-            
-        super().update(z, H, R)
 
     def _adapt_noise_covariances(self):
         """Estimates Q and R based on innovation sequence."""
-        # Estimated R (Measurement noise)
-        # R_hat = (1/N) * sum(y*y.T) - (1/N) * sum(H*P_minus*H.T)
         sum_yyT = np.zeros((self.dim_z, self.dim_z))
         sum_HPHT = np.zeros((self.dim_z, self.dim_z))
+        sum_Q = np.zeros((self.dim_x, self.dim_x))
         
         for i in range(self.N):
             y = self.innovations[i]
             H = self.H_list[i]
             Pm = self.P_minus_list[i]
+            dx = self.dx_list[i]
+            P_plus = self.P_plus_list[i]
+            FPF_T = self.FPF_T_list[i]
+            
             sum_yyT += np.outer(y, y)
             sum_HPHT += np.dot(np.dot(H, Pm), H.T)
             
+            # Myers-Tapley formula for Q
+            sum_Q += np.outer(dx, dx) + P_plus - FPF_T
+            
         R_hat = (1.0 / self.N) * sum_yyT - (1.0 / self.N) * sum_HPHT
-        
-        # Ensure R is positive definite
         self.R = self._make_psd(R_hat, self.dim_z)
         
-        # Estimated Q (Process noise) - Simplified Sage-Husa or Myers-Tapley
-        # Q_hat = (1/N) * sum(dx * dx.T) where dx is state correction
-        # For simplicity, we can also use the gain-based approach or residuals
-        # Here we use a common heuristic for Q adaptation
-        # dx = x_filtered - x_predicted
-        # sum_dxdxT = ...
-        
-        # (Optional: Implement full Q estimation if required)
-        pass
+        Q_hat = (1.0 / self.N) * sum_Q
+        self.Q = self._make_psd(Q_hat, self.dim_x)
 
     def _make_psd(self, matrix, dim):
         """Force a matrix to be positive semi-definite."""
